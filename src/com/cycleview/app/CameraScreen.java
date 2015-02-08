181648005c736c1b922efcff960a582cb8e464ca
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-02-08 14:04:54 -0300

    Correção de bug na detecção da Wifi

--- src/com/cycleview/app/CameraScreen.java
+++ src/com/cycleview/app/CameraScreen.java
@@ -7,7 +7,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,85 +15,108 @@ import com.cycleview.app.R;
 import com.gstreamer.GStreamer;
 
 public class CameraScreen extends Activity implements SurfaceHolder.Callback {
-    private native void nativeInit();     // Initialize native code, build pipeline, etc
-    private native void nativeFinalize(); // Destroy pipeline and shutdown native code
-    private native void nativePlay();     // Set pipeline to PLAYING
-    private native void nativePause();    // Set pipeline to PAUSED
-    private static native boolean nativeClassInit(); // Initialize native class: cache Method IDs for callbacks
-    private native void nativeSurfaceInit(Object surface);
-    private native void nativeSurfaceFinalize();
-    private long native_custom_data;      // Native code will use this to keep private data
+	// Initialize native code, build pipeline, etc
+	private native void nativeInit();
 
-    // Called when the activity is first created.
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
+	// Destroy pipeline and shutdown native code
+	private native void nativeFinalize();
 
-        // Initialize GStreamer and warn if it fails
-        try {
-            GStreamer.init(this);
-        } catch (Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
+	// Set pipeline to PLAYING
+	private native void nativePlay();
 
-        setContentView(R.layout.camerascreen);
+	// Set pipeline to PAUSED
+	private native void nativePause();
 
-        SurfaceView sv = (SurfaceView) this.findViewById(R.id.surface_video);
-        SurfaceHolder sh = sv.getHolder();
-        sh.addCallback(this);
+	// Initialize native class: cache Method IDs for callbacks
+	private static native boolean nativeClassInit();
 
-        nativeInit();
-        nativePlay();
-    }
+	private native void nativeSurfaceInit(Object surface);
 
-    protected void onSaveInstanceState (Bundle outState) {
-    }
+	private native void nativeSurfaceFinalize();
 
-    protected void onDestroy() {
-        nativeFinalize();
-        super.onDestroy();
-    }
+	// Native code will use this to keep private data
+	private long native_custom_data;
 
-    // Called from native code. This sets the content of the TextView from the UI thread.
-    private void setMessage(final String message) {
-//        final TextView tv = (TextView) this.findViewById(R.id.textview_message);
-//        runOnUiThread (new Runnable() {
-//          public void run() {
-//            tv.setText(message);
-//          }
-//        });
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    // Called from native code. Native code calls this once it has created its pipeline and
-    // the main loop is running, so it is ready to accept commands.
-    private void onGStreamerInitialized () {
-        Log.i ("GStreamer", "Gst initialized. Playing");
-        nativePlay();
-    }
+		// Initialize GStreamer and warn if it fails
+		try {
+			GStreamer.init(this);
+		} catch (Exception e) {
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
 
-    static {
-        System.loadLibrary("gstreamer_android");
-        System.loadLibrary("cycleview");
-        nativeClassInit();
-    }
+		setContentView(R.layout.camerascreen);
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-            int height) {
-        Log.d("GStreamer", "Surface changed to format " + format + " width "
-                + width + " height " + height);
-        nativeSurfaceInit (holder.getSurface());
-    }
+		final View settingsLayout = (View) this
+				.findViewById(R.id.layout_settings);
+		((Button) this.findViewById(R.id.button_settings))
+				.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
+						if (settingsLayout.getVisibility() == View.GONE) {
+							settingsLayout.setVisibility(View.VISIBLE);
+							((Button) v).setText(R.string.hide_settings);
+						} else {
+							settingsLayout.setVisibility(View.GONE);
+							((Button) v).setText(R.string.show_settings);
+						}
+					}
+				});
 
-    public void surfaceCreated(SurfaceHolder holder) {
-        Log.d("GStreamer", "Surface created: " + holder.getSurface());
-    }
+		SurfaceView sv = (SurfaceView) this.findViewById(R.id.surface_video);
+		sv.getHolder().addCallback(this);
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.d("GStreamer", "Surface destroyed");
-        nativeSurfaceFinalize ();
-    }
+		nativeInit();
+		nativePlay();
+	}
+
+	protected void onDestroy() {
+		nativeFinalize();
+		super.onDestroy();
+	}
+
+	// Called from native code. This sets the content of the TextView from the
+	// UI thread.
+	private void setMessage(final String message) {
+		final TextView tv = (TextView) this.findViewById(R.id.textview_status);
+		runOnUiThread(new Runnable() {
+			public void run() {
+				tv.setText(message);
+			}
+		});
+	}
+
+	// Called from native code. Native code calls this once it has created its
+	// pipeline and the main loop is running, so it is ready to accept commands.
+	private void onGStreamerInitialized() {
+		Log.i("GStreamer", "Gst initialized. Playing");
+		nativePlay();
+	}
+
+	static {
+		System.loadLibrary("gstreamer_android");
+		System.loadLibrary("cycleview");
+		nativeClassInit();
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		Log.d("GStreamer", "Surface changed to format " + format + " width "
+				+ width + " height " + height);
+		nativeSurfaceInit(holder.getSurface());
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d("GStreamer", "Surface created: " + holder.getSurface());
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.d("GStreamer", "Surface destroyed");
+		nativeSurfaceFinalize();
+	}
 
 }
--- src/com/cycleview/app/WifiScreen.java
+++ src/com/cycleview/app/WifiScreen.java
@@ -5,49 +5,54 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.SupplicantState;
 import android.os.Bundle;
+import android.os.Debug;
+import android.widget.Toast;
 
 public class WifiScreen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.wifiscreen);
 
 		final WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-		
+
 		if (wifiInfo != null) {
-			String name = wifiInfo.getSSID();
-			
+			String name = wifiInfo.getSSID().replace("\"", "");
+
 			if (name != null && name.equalsIgnoreCase(Constants.SSID))
-				wifiReady();	
+				wifiReady();
 		}
-		
+
 		// register for wifi change detection
 		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+		intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
 		registerReceiver(new BroadcastReceiver() {
 			@Override
 			public void onReceive(Context context, Intent intent) {
-				if (WifiManager.SUPPLICANT_STATE_CHANGED_ACTION.equals(intent.getAction())) {
-                    SupplicantState state = intent.getParcelableExtra(WifiManager.EXTRA_NEW_STATE);
-					if (SupplicantState.isValidState(state) && state == SupplicantState.COMPLETED) {
+				if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent
+						.getAction())) {
+					NetworkInfo nwInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+					// This implies the WiFi connection is through
+					if (NetworkInfo.State.CONNECTED.equals(nwInfo.getState())) {
 						WifiInfo newWifiInfo = wifiManager.getConnectionInfo();
-						if (newWifiInfo != null && newWifiInfo.getSSID().equalsIgnoreCase(Constants.SSID))
+						if (newWifiInfo != null
+								&& newWifiInfo.getSSID().replace("\"", "").equalsIgnoreCase(
+										Constants.SSID))
 							wifiReady();
 					}
 				}
 			}
 		}, intentFilter);
 	}
-	
+
 	protected void wifiReady() {
 		startActivity(new Intent(WifiScreen.this, CameraScreen.class));
 	}
 }
-
